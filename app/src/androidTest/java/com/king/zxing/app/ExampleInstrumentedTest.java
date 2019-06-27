61a40d70e7c54662106de62629aab507a3ba7c65
jenly1314 <jenly1314@mail.com>
2019-06-27 18:09:12 +0800

    支持AndroidX对应版本

--- app/src/androidTest/java/com/king/zxing/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/king/zxing/app/ExampleInstrumentedTest.java
@@ -1,13 +1,14 @@
 package com.king.zxing.app;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
