d325c1404e71bfb60f7a20e6ea64907ecaf5ba71
Jenly <jenly1314@gmail.com>
2024-08-18 15:42:33 +0800

    docs

--- build_docs.sh
+++ build_docs.sh
@@ -10,11 +10,14 @@ mv zxing-lite/build/dokka/html/* docs/api
 
 # Copy in special files that GitHub wants in the project root.
 GITHUB_URL=https://github.com/jenly1314/ZXingLite/
+echo $GITHUB_URL
 sed "/<!-- end -->/q" README.md > docs/index.md
-sed -i "s|app/src/main/ic_launcher-web.png|${GITHUB_URL}blob/master/app/src/main/ic_launcher-web.png|g" docs/index.md
-sed -i "s|[Image](GIF.gif)|[Image](${GITHUB_URL}blob/master/GIF.gif)|g" docs/index.md
-sed -i "s|[app](app)|[app](${GITHUB_URL}blob/master/app)|g" docs/index.md
+sed -i "s|app/src/main/ic_launcher-web.png|ic_logo.png|g" docs/index.md
+sed -i "s|(app)|(${GITHUB_URL}blob/master/app)|g" docs/index.md
 cat CHANGELOG.md | grep -v '## 版本日志' > docs/changelog.md
+
+cp GIF.gif docs/GIF.gif
+cp app/src/main/ic_launcher-web.png docs/ic_logo.png
 
 # Build the site locally
 mkdocs build
